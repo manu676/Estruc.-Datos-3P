c1bf8b76433279604e4d651770ecff2b3763a94e
MAU <42820040+manu676@users.noreply.github.com>
2019-11-06 11:25:11 -0600

    no hecho :)

---
+++ FIFO Circular/cola.js
@@ -0,0 +1,11 @@
+export default class Colas{
+    constructor(){
+        this._start = null;
+        this._textNoProcess = "";
+        this._textProcess = "";
+    }
+    addCola(number,timeProcess){
+        let nuevoTrabaho = new Work(number,timeProcess);
+    }
+    
+}
