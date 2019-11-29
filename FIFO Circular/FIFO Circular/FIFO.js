c0761682cc4e16c73f281d91cbbde5e9e0f11025
MAU <42820040+manu676@users.noreply.github.com>
2019-11-29 09:05:04 -0600

    Casi 80

--- FIFO Circular/FIFO Circular/FIFO.js
+++ FIFO Circular/FIFO Circular/FIFO.js
@@ -1,40 +1,77 @@
-export default class Fifo {
+export default class CircularListFIFO {
     constructor() {
         this._start = null;
+        this._turnoSiguiente = null;
         this._contador = 0;
     }
+
     get size() {
         return this._contador;
     }
-    push(node) {
+
+    get inTurn() {
+        return this._turnoSiguiente;
+    }
+
+    nextNode() {
+        if (this._turnoSiguiente != null) {
+            this._turnoSiguiente = this._turnoSiguiente.next;
+        }
+    }
+
+    push(data) {
         if (this._start != null) {
-            let temporal = this._start;
-            while (temporal.next != null)
-                temporal = temporal.next;
-            temporal.next = node;
-        } else
-            this._start = node;
+            let aux = this._start;
+            while (aux.next != this._start)
+                aux = aux.next;
+                aux.next = data;
+                data.next = this._start;
+        } else {
+            this._start = data;
+            this._start.next = this._start;
+            this._turnoSiguiente = this._start;
+        }
+
         this._contador++;
     }
+
     pop() {
-        let temporal = null;
-        if (this._start != null) {
-            temporal = this._start;
-            this._start = this._start.next;
-        }
+        if (this._turnoSiguiente != this._start) {
+            let aux = this._start;
+            while (aux.next != this._turnoSiguiente)
+                aux = aux.next
+            aux.next = aux.next.next;
+            this._turnoSiguiente = aux.next.next;
+        } else
+            this._start = null;
+
         this._contador--;
-        return temporal;
     }
-    peek() {
-        return this._start;
-    }
-    print() {
-        let temporal = this._start;
-        let string = '';
-        while (temporal != null) {
-            string = string + '\n' + temporal.toString();
-            temporal = temporal.next;
+    recordTurns () {
+        let string = "";
+        if (this._start != null) {
+            let aux = this._start;
+            string = aux.toString();
+            aux = aux.next;
+            while (aux != this._start) {
+                string = string + "\n" + aux.toString();
+                aux = aux.next;
+            }
         }
         return string;
+    }
+
+    TiempoRequerido(){
+        let tiempoTotal = 0;
+        if (this._start != null) {
+            let aux = this._start;
+            tiempoTotal = aux.timeRequired;
+            aux = aux.next;
+            while (aux != this._start) {
+                tiempoTotal += aux.timeRequired;
+                aux = aux.next;
+            }
+        }
+        return tiempoTotal;
     }
 }
